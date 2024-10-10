7af7e29360f4b453bbdf6b847dda4fd4edab1ded
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-10-10 20:37:55 +0530

    Update yaap_blossom.mk

--- yaap_blossom.mk
+++ yaap_blossom.mk
@@ -12,15 +12,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
-# Inherit some common YAAP stuff.
-$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+# Inherit some common ProjectBlaze-15 stuff.
+$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
 
-# include private signing keys
--include vendor/niigo-priv/keys/keys.mk
+#ProjectBlaze-14 stuffs.
+BLAZE_MAINTAINER := AsTechpro20
+TARGET_SUPPORTS_BLUR := true
+TARGET_ENABLE_BLUR := true
 
 scr_resolution := 720
 
-PRODUCT_NAME := yaap_blossom
+PRODUCT_NAME := blaze_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
