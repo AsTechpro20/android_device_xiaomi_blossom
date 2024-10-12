ffba1ac3bdf378278c5f857b853be268e82514ae
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-10-12 09:39:18 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_blossom.mk
+    $(LOCAL_DIR)/derp_blossom.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_blossom-user \
-    blaze_blossom-userdebug \
-    blaze_blossom-eng
+    derp_blossom-user \
+    derp_blossom-userdebug \
+    derp_blossom-eng
