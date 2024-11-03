935e4584f7afc6fcee50d5076692f1a816846d88
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-11-03 21:15:37 +0530

    Update and rename derp_blossom.mk to infinity_blossom.mk

--- infinity_blossom.mk
+++ infinity_blossom.mk
@@ -12,15 +12,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
-# Inherit some common DerpFest-15 stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common Infinity-X-15 stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
+
+# Infinity-X-15 stuffs.
+INFINITY_MAINTAINER := AsTechpro20
 TARGET_SUPPORTS_BLUR := true
 TARGET_ENABLE_BLUR := true
 scr_resolution := 720
-TARGET_USES_PICO_GAPPS := true
 
-PRODUCT_NAME := derp_blossom
+PRODUCT_NAME := infinity_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
