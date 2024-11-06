fc9f3bac06a3c8029ef6d07f91582e337287b314
AsTechpro20 <94213632+AsTechpro20@users.noreply.github.com>
2024-11-06 17:02:53 +0530

    Update and rename infinity_blossom.mk to lineage_blossom.mk

--- lineage_blossom.mk
+++ lineage_blossom.mk
@@ -12,17 +12,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
-# Inherit some common Infinity-X-15 stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+# Inherit some common RisingOS-15 stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 
-# Infinity-X-15 stuffs.
-INFINITY_MAINTAINER := AsTechpro20
+# RisingOS-15 stuffs.
+RISING_MAINTAINER="AsTechpro20"
 TARGET_SUPPORTS_BLUR := true
 TARGET_ENABLE_BLUR := true
+PRODUCT_NO_CAMERA := true
 scr_resolution := 720
 
-PRODUCT_NAME := infinity_blossom
+PRODUCT_NAME := lineage_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
